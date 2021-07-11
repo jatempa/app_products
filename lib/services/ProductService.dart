6fd74252ab9cf1ca146138b8ad32c457ec04a191
Jorge Atempa <atempaitm@gmail.com>
2021-07-11 01:11:21 -0700

    Add 'Product' service

---
+++ lib/services/ProductService.dart
@@ -0,0 +1,21 @@
+import 'package:app_products/api/api.dart';
+import 'package:app_products/models/Product.dart';
+
+class ProductService {
+  final _dio = Api().dio;
+
+  Future<List<Product>?> fetchProducts() async {
+    try {
+      final response = await _dio.get('products');
+
+      if (response.statusCode == 200 && response.data != null)
+        return (response.data as List)
+          .map((p) => Product.fromJson(p))
+          .toList();
+    } catch (error) {
+      print(error);
+    }
+
+    return [];
+  }
+}
