b294d0ed598f44aa6ad6ed679b69a8ac708bea25
Jorge Atempa <atempaitm@gmail.com>
2021-10-02 21:57:04 -0700

    Convert to stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
