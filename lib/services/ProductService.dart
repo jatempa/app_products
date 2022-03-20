b41620683bb1219bf39a43ba663ecf1d34d38eea
Jorge Atempa <atempaitm@gmail.com>
2022-03-19 20:12:19 -0700

    Import 'Api' singleton

--- lib/services/ProductService.dart
+++ lib/services/ProductService.dart
@@ -1,14 +1,15 @@
 import 'dart:developer';
 
 import 'package:app_products/api/api.dart';
+import 'package:app_products/di/app.locator.dart';
 import 'package:app_products/models/Product.dart';
 
 class ProductService {
-  final _dio = Api().dio;
+  final _api = locator<Api>();
 
   Future<List<Product>?> fetchProducts() async {
     try {
-      final response = await _dio.get('products');
+      final response = await _api.dio.get('products');
 
       if (response.statusCode == 200 && response.data != null) {
         return (response.data as List)
