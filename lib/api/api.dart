11b7d525d490a35b2ede87fc5d8586185830ee20
Jorge Atempa <atempaitm@gmail.com>
2022-03-19 19:24:21 -0700

    Create singleton object with 'get_it' package

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,26 +1,10 @@
 import 'package:dio/dio.dart';
 
 class Api {
-  static final Api _singleton = new Api._internal();
-  static const baseUrl = 'https://fakestoreapi.com/';
-
-  factory Api() {
-    return _singleton;
-  }
-
-  Api._internal();
-
   Dio get dio {
-    Dio dio = new Dio();
-    dio.interceptors.add(InterceptorsWrapper(
-      onRequest: (options, handler) {
-        //set base url
-        options.baseUrl = '$baseUrl';
-
-        return handler.next(options);
-      }
-    ));
-
-    return dio;
+    final _dio = Dio();
+    //set base url
+    _dio.options.baseUrl = 'https://fakestoreapi.com/';
+    return _dio;
   }
 }
